701bd25190a40eddbf575b2808b0a437a82c566b
DarkMantisCS <darkmantis@cybershade.org>
2014-06-29 20:40:15 +0100

    Submitting changes

--- main.js
+++ main.js
@@ -19,21 +19,26 @@ function main() {
     if(ircBot != null) {
 
         // Set up the event listeners
+
+        // First connection loop to the server
         ircBot.addListener("registered", function(message) {
             console.log(message);
         });
 
+        // Log out the motd
         ircBot.addListener("motd", function(message) {
             console.log(message);
         });
 
+        // Catch the errors
         ircBot.addListener("error", function(messages) {
             //messages is an object?
-            for(var i = 0; i < messages.length; i++) {
-                console.log("ERROR " + messages[i]);
-            }
+            // for(var i = 0; i < messages.length; i++) {
+            //     console.log("ERROR " + messages[i]);
+            // }
         });
 
+        // Welcome users on join
         ircBot.addListener("join", function(channel, nick) {
             if(nick == config[0].botName) {
                 ircBot.say(channel, "Hola!");
@@ -42,10 +47,12 @@ function main() {
             }
         });
 
+        // Auto-join on invite
         ircBot.addListener("invite", function(channel, from, message) {
             ircBot.join(channel);
         });
 
+        // On connection end
         ircBot.addListener("end", function() {
             console.log(arguments);
         });
